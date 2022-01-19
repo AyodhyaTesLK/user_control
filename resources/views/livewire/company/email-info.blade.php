4089c939fc583b4ea5b1a593ef3efb796d641143
AyodhyaTesLK <ayodhya.teslk@gmail.com>
2022-01-19 20:26:08 +0530

    2022.01.19

--- resources/views/livewire/company/email-info.blade.php
+++ resources/views/livewire/company/email-info.blade.php
@@ -7,7 +7,7 @@
         @if($config_item['batch_no'] == 4)
             <div class="w-full py-2">
                 <label for="{{ $config_item['key'] }}" class="block text-sm font-medium text-gray-700">{{ $config_item['lable'] }}
-                    <span class="font-thin px-1">(optional)</span>
+                    @if(!$config_item['is_required'])<span class="font-thin px-1">(optional)</span>@endif
                 </label>
                 <input wire:model.lazy="configs.{{ $key }}.value" type="{{ $config_item['type'] }}" name="{{ $config_item['key'] }}" id="{{ $config_item['key'] }}"
                     class="px-2 py-1.5 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-sm">
